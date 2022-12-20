346ff91b0154e8195d1c862d72834c808285e0bf
kalininaekaterina2004 <kalinina.ekaterina@hexly.ru>
2022-12-20 14:48:20 +0300

    .

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
-const starting = (text) => {
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-console.log(text);
-return userName;
+
+const welcome = (text) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(text);
+  return userName;
 };
-export default starting;
+export default welcome;
