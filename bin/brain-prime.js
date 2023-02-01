bd7662d109886064b76adeed88faac0390049120
kalininaekaterina2004 <kalinina.ekaterina@hexly.ru>
2023-02-01 12:33:28 +0300

    .

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,7 +6,7 @@ import { magicNumbers } from '../src/math.js';
 const prime = () => {
   const userName = welcome('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
-    const random = magicNumbers(1, 1368);
+    const random = magicNumbers(1, 1367);
     const b = magicNumbers();
     const c = b !== random;
     const example = `${random}`;
