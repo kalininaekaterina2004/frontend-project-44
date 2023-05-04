16f1417560b196976813a45b380ae68575a49c69
kalininaekaterina2004 <kalinina.ekaterina@hexly.ru>
2023-05-04 11:14:47 +0300

    fix7

--- src/math.js
+++ src/math.js
@@ -1,1 +1,2 @@
-export const getRandomInRange = (min = 0, max = 70) => Math.floor(min + Math.random() * (max - min + 1));
+const getRandomInRange = (min = 0, max = 70) => Math.floor(min + Math.random() * (max - min + 1));
+export default getRandomInRange;
